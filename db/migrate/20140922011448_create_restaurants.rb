acfdd18cfd6aacf1d383f31a0897d5ba0c445369
Josh <rkb.josh@gmail.com>
2014-09-21 22:22:13 -0400

    added tables for restaurants and reviews. Can't figure out the MVC, therefore can't figure out preinting things to the page.

---
+++ db/migrate/20140922011448_create_restaurants.rb
@@ -0,0 +1,16 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name, :null => false
+      t.text :description
+      t.string :address, :null => false
+      t.string :city, :null => false
+      t.string :state, :null => false
+      t.integer :zip_code, :null => false
+      t.string :category
+
+      t.timestamps
+
+    end
+  end
+end
